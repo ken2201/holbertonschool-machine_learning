face1af4c54edecf753ab190fba83742ba95541f
ken2201 <ndayishimiyeken22@gmail.com>
2026-02-21 15:58:53 +0100

    Add transition layer implementation for deep CNNs

---
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Transition Layer"""
+from tensorflow import keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """
+    Returns: The output of the transition layer
+    and the number of filters within the output,
+    respectively.
+    """
+    he_normal = K.initializers.HeNormal(seed=0)
+
+    filters = int(nb_filters * compression)
+
+    X = K.layers.BatchNormalization()(X)
+    X = K.layers.ReLU()(X)
+    X = K.layers.Conv2D(filters, (1, 1),
+                        padding='same',
+                        kernel_initializer=he_normal)(X)
+
+    X = K.layers.AveragePooling2D(pool_size=2, strides=2,
+                                  padding='same')(X)
+
+    return X, filters
