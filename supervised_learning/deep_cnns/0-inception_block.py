f8afd8ea3187ebd9fbe63fe9db3c076076756d0e
ken2201 <ndayishimiyeken22@gmail.com>
2026-02-21 15:56:30 +0100

    Add inception block and network implementation for deep CNNs

---
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Inception Block"""
+from tensorflow import keras as K
+
+
+def inception_block(A_prev, filters):
+    """Returns: the concatenated output of the inception block."""
+
+    F1, F3R, F3, F5R, F5, FPP = filters
+
+    f1_conv = K.layers.Conv2D(F1, (1, 1), padding='same',
+                              activation='relu')(A_prev)
+
+    f3r_conv_reduce = K.layers.Conv2D(F3R, (1, 1), padding='same',
+                                      activation='relu')(A_prev)
+    f3_conv = K.layers.Conv2D(F3, (3, 3), padding='same',
+                              activation='relu')(f3r_conv_reduce)
+
+    f5r_conv_reduce = K.layers.Conv2D(F5R, (1, 1), padding='same',
+                                      activation='relu')(A_prev)
+    f5_conv = K.layers.Conv2D(F5, (5, 5), padding='same',
+                              activation='relu')(f5r_conv_reduce)
+
+    max_pooling = K.layers.MaxPooling2D((3, 3), strides=(1, 1),
+                                        padding='same')(A_prev)
+    conv_after_pool = K.layers.Conv2D(FPP, (1, 1),
+                                      padding='same',
+                                      activation='relu')(max_pooling)
+
+    output = K.layers.Concatenate(axis=-1)([f1_conv, f3_conv,
+                                            f5_conv, conv_after_pool])
+
+    return output
